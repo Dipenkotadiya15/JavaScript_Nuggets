455ac9ddd5bd64e1779f512d41fe6405ac085263
Dipenkotadiya15 <dipen.adsum@gmail.com>
2023-01-18 11:37:15 +0530

    Map in js

---
+++ js/01_Map.js
@@ -0,0 +1,25 @@
+// Map method 
+const people = [
+    {
+        name: 'bob', 
+        age: 20, 
+        position: 'developer',
+    },
+    {
+        name: 'anna', 
+        age: 25, 
+        position: 'designer',
+    },
+    {
+        name: 'susy', 
+        age: 30, 
+        position: 'the boss',
+    },
+];
+
+
+
+// Quokka.js - Extension
+// Returns a New Array
+// does not change the size of original array (unlike filter)
+// uses values from original array when making new one 
